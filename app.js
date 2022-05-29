ca87da6a43daf701b188b86cf84c7743e7e3bf8c
maliabarker <89502882+maliabarker@users.noreply.github.com>
2022-05-29 13:35:57 -1000

    added reviews route and template

--- app.js
+++ app.js
@@ -7,8 +7,21 @@ var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
+let reviews = [
+    { title: "Great Review", movieTitle: "Batman II" },
+    { title: "Awesome Movie", movieTitle: "Titanic" }
+]
+
+// ROUTES
+
+// hello world route
+// app.get('/', (req, res) => {
+//     res.render('home', { msg: 'Handlebars are Cool!' });
+// })
+
+// reviews (index) route
 app.get('/', (req, res) => {
-    res.render('home', { msg: 'Handlebars are Cool!' });
+    res.render('reviews-index', { reviews: reviews });
 })
 
 app.listen(3000, () => {
