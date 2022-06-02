5fff7c248d6e0449dae3fcf96168b6afeb952936
maliabarker <89502882+maliabarker@users.noreply.github.com>
2022-06-01 14:49:45 -1000

    Added mongodb and review model

--- app.js
+++ app.js
@@ -4,13 +4,21 @@ const app = express()
 
 var exphbs = require('express-handlebars');
 
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/rotten-potatoes', { useNewUrlParser: true });
+
+const Review = mongoose.model('Review', {
+    title: String,
+    movieTitle: String
+});
+
 app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 
-let reviews = [
-    { title: "Great Review", movieTitle: "Batman II" },
-    { title: "Awesome Movie", movieTitle: "Titanic" }
-]
+// let reviews = [
+//     { title: "Great Review", movieTitle: "Batman II" },
+//     { title: "Awesome Movie", movieTitle: "Titanic" }
+// ]
 
 // ROUTES
 
@@ -19,10 +27,21 @@ let reviews = [
 //     res.render('home', { msg: 'Handlebars are Cool!' });
 // })
 
-// reviews (index) route
+// INDEX
 app.get('/', (req, res) => {
-    res.render('reviews-index', { reviews: reviews });
+    Review.find()
+      .then(reviews => {
+        res.render('reviews-index', { reviews: reviews });
+      })
+      .catch(err => {
+        console.log(err);
+      })
 })
+
+// reviews (index) route
+// app.get('/', (req, res) => {
+//     res.render('reviews-index', { reviews: reviews });
+// })
 
 app.listen(3000, () => {
   console.log('App listening on port 3000!')
