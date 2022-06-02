a0f31c738cd859c2ea706d97defce77e5c1ad801
maliabarker <89502882+maliabarker@users.noreply.github.com>
2022-06-01 15:12:41 -1000

    users can create reviews

--- app.js
+++ app.js
@@ -9,8 +9,12 @@ mongoose.connect('mongodb://localhost/rotten-potatoes', { useNewUrlParser: true 
 
 const Review = mongoose.model('Review', {
     title: String,
+    description: String,
     movieTitle: String
 });
+
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
@@ -29,13 +33,29 @@ app.set('view engine', 'handlebars');
 
 // INDEX
 app.get('/', (req, res) => {
-    Review.find()
+    Review.find().lean()
       .then(reviews => {
         res.render('reviews-index', { reviews: reviews });
+        // console.log(reviews)
       })
       .catch(err => {
         console.log(err);
       })
+})
+
+// NEW REVIEW
+app.get('/reviews/new', (req, res) => {
+    res.render('reviews-new', {});
+})
+
+// CREATE
+app.post('/reviews', (req, res) => {
+    Review.create(req.body).then((review) => {
+      console.log(review);
+      res.redirect('/');
+    }).catch((err) => {
+      console.log(err.message);
+    })
 })
 
 // reviews (index) route
