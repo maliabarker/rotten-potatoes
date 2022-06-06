b4ac5cf2224f184f0877da07ee712f8eece84af8
maliabarker <89502882+maliabarker@users.noreply.github.com>
2022-06-05 17:14:41 -1000

    added time stamps, footer

--- models/review.js
+++ models/review.js
@@ -1,26 +1,16 @@
 // models/review.js
 
 const mongoose = require('mongoose');
-// mongoose.connect('mongodb://localhost/rotten-potatoes', { useNewUrlParser: true });
 
-// const uri = "mongodb+srv://maliabarker:supersecretpassword@cluster0.mnou0t0.mongodb.net/?retryWrites=true&w=majority";
-// try {
-//     // Connect to the MongoDB cluster
-//      mongoose.connect(
-//       uri,
-//       { useNewUrlParser: true, useUnifiedTopology: true },
-//       () => console.log(" Mongoose is connected")
-//     );
-
-//   } catch (e) {
-//     console.log("could not connect");
-// }
-
-const Review = mongoose.model('Review', {
+const Review = mongoose.model('Review', new mongoose.Schema({
     title: String,
     description: String,
     movieTitle: String,
     movieRating: Number
-});
+}, {
+    timestamps: true
+}));
+
+// const Review = mongoose.model('Review', reviewScheme);
 
 module.exports = Review;
