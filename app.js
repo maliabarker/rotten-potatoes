2a3dc0547a05deafa5a8bbbc3feb1b58f1e3078e
maliabarker <89502882+maliabarker@users.noreply.github.com>
2022-06-05 16:25:26 -1000

    trying to fix heroku attempt 2: connecting to mongoose

--- app.js
+++ app.js
@@ -19,16 +19,6 @@ const reviews = require('./controllers/reviews')(app);
 module.exports = app;
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri = "mongodb+srv://maliabarker:supersecretpassword@cluster0.mnou0t0.mongodb.net/?retryWrites=true&w=majority";
-const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-  const collection = client.db("test").collection("devices");
-  // perform actions on the collection object
-  client.close();
-});
-
-
 // let reviews = [
 //     { title: "Great Review", movieTitle: "Batman II" },
 //     { title: "Awesome Movie", movieTitle: "Titanic" }
--- models/review.js
+++ models/review.js
@@ -1,15 +1,29 @@
 // models/review.js
 
-// const mongoose = require('mongoose');
-
-// const Review = mongoose.model('Review', {
-//   title: String,
-//   description: String,
-//   movieTitle: String
+const mongoose = require('mongoose');
+// mongoose.connect('mongodb://localhost/rotten-potatoes', { useNewUrlParser: true });
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const { default: mongoose } = require('mongoose');
+const uri = "mongodb+srv://maliabarker:supersecretpassword@cluster0.mnou0t0.mongodb.net/?retryWrites=true&w=majority";
+// const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// client.connect(err => {
+//   const collection = client.db("test").collection("devices");
+//   // perform actions on the collection object
+//   client.close();
 // });
 
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/rotten-potatoes', { useNewUrlParser: true });
+// mongoose.connect(uri)
+try {
+    // Connect to the MongoDB cluster
+     mongoose.connect(
+      uri,
+      { useNewUrlParser: true, useUnifiedTopology: true },
+      () => console.log(" Mongoose is connected")
+    );
+
+  } catch (e) {
+    console.log("could not connect");
+}
 
 const Review = mongoose.model('Review', {
     title: String,
